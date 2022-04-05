d929986205496ac629a5a40c9994f25ce68af6eb
Russ Hewgill <russhewgill@gmail.com>
2022-04-05 12:51:34 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,57 @@
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 // use cortex_m::asm;
+use cortex_m::{iprintln, peripheral::ITM};
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
+use stm32f4::stm32f401;
+
+#[inline(never)]
+fn delay(tim9: &stm32f401::tim9::RegisterBlock, ms: u16) {
+    unsafe {
+        tim9.arr.write(|w| w.arr().bits(ms));
+    }
+
+    tim9.cr1.modify(|_, w| w.cen().set_bit());
+
+    while !tim9.sr.read().uif().bit_is_set() {}
+
+    tim9.sr.modify(|_, w| w.uif().clear_bit());
+}
+
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!");
+    // hprintln!("Hello, world!, {}", 1);
+
+    // let mut cp = stm32f401::CorePeripherals::take().unwrap();
+    let mut ps = stm32f401::Peripherals::take().unwrap();
+
+    ps.RCC.apb2enr.modify(|_, w| w.tim9en().set_bit());
+
+    ps.TIM9.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+    ps.TIM9.psc.write(|w| w.psc().bits(7_999));
+
+    hprintln!("wat 0");
+    delay(&ps.TIM9, 1000);
+
+    hprintln!("wat 1");
+    delay(&ps.TIM9, 1000);
+
+    hprintln!("wat 2");
+
+    // cp.DCB.enable_trace();
+
+    // let swo_freq = 8_000_000;
+    // cp.TPIU.acpr
+
+    // enable stimulus port 0
+    // cp.ITM.ter[0].write(1);
+
+    // iprintln!(&mut cp.ITM.stim[0], "wat");
+
+    // let mut itm = init();
+    // iprintln!(&mut itm.stim[0], "wat");
 
     // // exit QEMU
     // // NOTE do not run this on hardware; it can corrupt OpenOCD state
@@ -29,4 +74,9 @@ fn main() -> ! {
     // loop {
     //     // your code goes here
     // }
+}
+
+pub fn init() -> ITM {
+    let p = cortex_m::Peripherals::take().unwrap();
+    p.ITM
 }
